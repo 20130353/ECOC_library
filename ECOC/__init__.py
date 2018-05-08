faa23ca778d809c7318e4b150e1428603a5aabba
mldmxm <34806301+mldmxm@users.noreply.github.com>
2018-05-08 08:11:51 +0800

    origin codes

---
+++ ECOC/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author: sunmengxin
+# time: 2018/1/30 17:06
+# file: __init__.py.py
+# description:
+
+import ECOCDemo.ECOC.Classifier
+import ECOCDemo.ECOC.Criterion
+import ECOCDemo.ECOC.Distance
+import ECOCDemo.ECOC.Greedy_Search
+import ECOCDemo.ECOC.Matrix_tool
+import ECOCDemo.ECOC.SFFS
---
+++ teste.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.arange(0.885,1.010,0.005)
+print(x)
